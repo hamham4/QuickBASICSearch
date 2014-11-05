56728bce5895ff549bd93e919fa858d9cb5f6c51
Sara Thorup <thorups@gmail.com>
2014-11-04 20:20:29 -0800

    result class created

---
+++ Result.java
@@ -0,0 +1,36 @@
+package fastNFA;
+
+import java.net.URL;
+
+public class Result {
+	private int numResults;
+	private URL searchURL;
+	private String searchTerm;
+	
+	public Result(String searchTerm, URL searchURL) {
+		this.searchTerm = searchTerm;
+		this.searchURL = searchURL;
+	}
+
+	public int getNumResults() {
+		return numResults;
+	}
+
+	public void setNumResults(int numResults) {
+		this.numResults = numResults;
+	}
+
+	public URL getSearchURL() {
+		return searchURL;
+	}
+
+	public String getSearchTerm() {
+		return searchTerm;
+	}
+	
+	
+	
+	
+	
+
+}
