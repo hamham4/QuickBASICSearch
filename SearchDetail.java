de4a7c96f3e8724cdbbbc724cefba871312076b2
Sara Thorup <thorups@gmail.com>
2014-11-23 16:36:20 -0800

    created searcher class

--- SearchDetail.java
+++ SearchDetail.java
@@ -6,6 +6,8 @@ public class SearchDetail {
 	private int numResults;
 	private URL searchURL;
 	private String searchTerm;
+	private boolean searched = false;
+	private boolean mandatory = true;
 	
 	public SearchDetail(String searchTerm, URL searchURL) {
 		this.searchTerm = searchTerm;
@@ -26,6 +28,22 @@ public class SearchDetail {
 
 	public String getSearchTerm() {
 		return searchTerm;
+	}
+
+	public boolean wasSearched() {
+		return searched;
+	}
+
+	public void setSearched(boolean searched) {
+		this.searched = searched;
+	}
+
+	public boolean isMandatory() {
+		return mandatory;
+	}
+
+	public void setMandatory(boolean mandatory) {
+		this.mandatory = mandatory;
 	}
 	
 }
---
+++ Searcher.java
@@ -0,0 +1,32 @@
+package fastNFA;
+
+import java.net.URL;
+
+public class Searcher {
+	
+	private int MAX_RESULTS = 15;
+	
+	private Searcher() {	
+	}
+	
+	public static void search(Searchable searchObject) {
+		//Iterate through search details
+		//Run search if mandatory = true
+		//Stop search if results < 15\
+		//Execute search URL
+		//Update search boolean
+		
+		//Parse the number of search results
+		//update search result count in searchURL
+		
+	}
+	
+	
+	private static int getResultCount(URL searchURL) {
+		
+		
+	}
+	
+	
+
+}
